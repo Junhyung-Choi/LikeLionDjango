384a3b4afd748f7c98d333aecb01a02e16a9dfb0
Junhyung-Choi <junhyung9571@naver.com>
2021-05-08 16:34:17 +0900

    ADD Visitor on Model

--- liondjnago/tiger/admin.py
+++ liondjnago/tiger/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Blog,Visitor
 
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Visitor)
--- liondjnago/tiger/models.py
+++ liondjnago/tiger/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    writer = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+class Visitor(models.Model):
+    visitor_id = models.CharField(max_length=100)
+    visitor_password = models.CharField(max_length=100)
+    visitor_signupdate = models.DateField()
