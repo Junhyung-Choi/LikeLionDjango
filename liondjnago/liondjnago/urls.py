f7d536447135e2fadb0150298c89eb86e65558f6
Junhyung-Choi <junhyung9571@naver.com>
2021-05-08 16:09:40 +0900

    ADD tiger application

--- liondjnago/liondjnago/urls.py
+++ liondjnago/liondjnago/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import tiger.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',tiger.views.home, name='home'),
 ]
